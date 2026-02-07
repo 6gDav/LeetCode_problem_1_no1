ee3f0a6086f42fa33df0cb0a4d321e64b714b3cf
6gDav <c6gdavprog0402@gmail.com>
2026-02-07 11:31:01 +0100

    Duplication bug fixed

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ mod solution;
 use solution::Solution as sol;
 
 fn main() {
-    println!("{:?}", sol::two_sum(vec![1, 5, 4, 7, 9], 12));
+    println!("{:?}", sol::two_sum(vec![6, 5, 4, 7, 9], 12));
 }
--- src/solution.rs
+++ src/solution.rs
@@ -1,21 +1,14 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut indexer_up = 0;
-
-        for _ in nums.iter() {
-            let mut indexer_lo = 0;
-
-            for _ in nums.iter() {
-                if nums[indexer_up] + nums[indexer_lo] == target {
-                    return vec![indexer_up as i32, indexer_lo as i32];
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        for (i, &num1) in nums.iter().enumerate() {
+            for (j, &num2) in nums.iter().enumerate().skip(i + 1) {
+                if num1 + num2 == target {
+                    return vec![i as i32, j as i32];
                 }
-                indexer_lo += 1;
             }
-            indexer_up += 1;
         }
-
-        vec![0]
+        vec![] 
     }
 }
