c85566ed5202a9c06f7f04c3c030b3cdd9927bb1
6gDav <c6gdavprog0402@gmail.com>
2026-02-07 10:28:20 +0100

    Simple clippy executed on the code and fmt as well

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,6 @@
-fn main() 
-{
-    println!("{:?}", Solution::two_sum(vec![1, 5, 4, 7, 9], 12));
-}
+mod solution;
+use solution::Solution as sol;
 
-struct Solution {}  
-
-impl Solution 
-{
-    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> 
-    {
-        let mut indexer_up = 0;
-        
-        for _ in nums.iter()
-        {
-            let mut indexer_lo = 0;
-
-            for _ in nums.iter() 
-            {
-                if nums[indexer_up] + nums[indexer_lo] == target
-                {
-                    return vec![indexer_up as i32, indexer_lo as i32];            
-                }
-                indexer_lo+=1;
-            }
-            indexer_up+=1;
-        }
-
-        return vec![0];
-    }
+fn main() {
+    println!("{:?}", sol::two_sum(vec![1, 5, 4, 7, 9], 12));
 }
---
+++ src/solution.rs
@@ -0,0 +1,21 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut indexer_up = 0;
+
+        for _ in nums.iter() {
+            let mut indexer_lo = 0;
+
+            for _ in nums.iter() {
+                if nums[indexer_up] + nums[indexer_lo] == target {
+                    return vec![indexer_up as i32, indexer_lo as i32];
+                }
+                indexer_lo += 1;
+            }
+            indexer_up += 1;
+        }
+
+        vec![0]
+    }
+}
